284a0db180dd341797c4c5224d4ad7b6b5738d26
Michael V <bolsunovskiy@gmail.com>
2015-08-28 00:34:46 +0300

    add some buttons for todo

--- views/beacon/list.php
+++ views/beacon/list.php
@@ -8,7 +8,9 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 
 <p>
-<a href="<?= Url::toRoute(['/beacon/add']) ?>" class="btn btn-primary">Add iBeacon</a>
+    <a href="<?= Url::toRoute(['/beacon/add']) ?>" class="btn btn-primary">Add iBeacon</a>
+    <button disabled class="btn btn-default">Import from kontakt.io</button>
+    <button disabled class="btn btn-default">Import from Estimote</button>
 </p>
 
 
